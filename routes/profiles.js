e299ece0e3d64ba1d55ed938e8922295cdded2be
awatersny <awatersny@gmail.com>
2022-03-06 14:20:19 -0500

    Test profiles route

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
+import * as profilesCtrl from "../controllers/profiles.js"
 
 const router = Router();
 
-
+router.get('/', profilesCtrl.index)
 
 export {
   router
